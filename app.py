d302d8ff5abc59cb18089f7806bdfc87305ae065
Reinaldo Chaves <reichaves@gmail.com>
2022-01-06 16:42:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ app = Flask(__name__)
 @app.route("/telegram", methods=["POST"])
 
 def telegram():
-  # Processa mensagem
+	# Processa mensagem
 	update = request.json
 	chat_id = update["message"]["chat"]["id"]
 	text = update["message"]["text"].lower()
 	
-  if text in ['oi', 'olá', 'ola']:
+  	if text in ['oi', 'olá', 'ola']:
 		answer = "Oi, como vai?!"
-	elif text in ['bom dia', 'boa tarde', 'boa noite']:
+  	elif text in ['bom dia', 'boa tarde', 'boa noite']:
 		answer = text
 	elif "jornalismo" in text:
 		answer = "Em breve..."
 	else:
 		answer = "Não entendi!!!"
 	
-  # Responde mensagem
+  	# Responde mensagem
 	token = os.environ["TELEGRAM_TOKEN"]
 	message = {"chat_id": chat_id, "text": answer}
 	url = f"https://api.telegram.org/bot{token}/sendMessage"
