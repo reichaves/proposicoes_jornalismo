58532b314db22b93d978f55552e8c34e36572a0f
Reinaldo Chaves <reichaves@gmail.com>
2022-01-11 17:05:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,53 +12,13 @@ import io
 import os
 import datetime
 
-app = dash.Dash(external_stylesheets = [ dbc.themes.FLATLY],)
 
-
-
-PLOTLY_LOGO = "https://www.portaldosjornalistas.com.br/wp-content/uploads/2019/09/Abraji.png"
-
-
-navbar = dbc.Navbar(
-        [
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src = PLOTLY_LOGO, height = "70px"), ),
-                        
-                        dbc.Col(
-             dbc.NavbarBrand("App Title", style = {'color':'black', 'fontSize':'25px','fontFamily':'Times New Roman'}
-                            ),
-             ),
-
-                    ],
-                    align="center",
-                    className="g-10",
-                ),
-            
-            dbc.Row(
-            [
-        dbc.Col(
-        dbc.Button(id = 'button', children = "Click Me!", color = "primary"), 
-            )        
-    ],
-            # add a top margin to make things look nice when the navbar
-            # isn't expanded (mt-3) remove the margin on medium or
-            # larger screens (mt-md-0) when the navbar is expanded.
-            # keep button and search box on same row (flex-nowrap).
-            # align everything on the right with left margin (ms-auto).
-     className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
-)
-            
-         ],
-)
-
-
-app.layout = html.Div(id = 'parent', children = [navbar])
+app = Flask(__name__)
 
 @app.route("/telegram", methods=["POST"])
 
 def telegram():
-        # Processa mensagem
+	# Processa mensagem
 	update = request.json
 	chat_id = update["message"]["chat"]["id"]
 	text = update["message"]["text"].lower()
@@ -82,10 +42,4 @@ def telegram():
 		raise RuntimeError("Erro ao responder API do Telegram")
 	
 	return "ok"
-
-
-
-if __name__ == "__main__":
-    app.run_server()
-
 
