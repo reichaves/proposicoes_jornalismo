f78b05857daabc746c8072652a1107b04990d89a
Reinaldo Chaves <reichaves@gmail.com>
2022-01-11 16:38:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-import datetime
-import io
+import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
 import requests
-from flask import Flask, render_template, request
+import pandas as pd
+import dash_core_components as dcc
+import plotly.express as px
+import numpy as np
+from dash.dependencies import Input,Output
+import dash_table
+import io
 import os
+import datetime
 
-app = Flask(__name__)
+app = dash.Dash(external_stylesheets = [ dbc.themes.FLATLY],)
+
 
 @app.route("/telegram", methods=["POST"])
 
@@ -33,3 +42,51 @@ def telegram():
 		raise RuntimeError("Erro ao responder API do Telegram")
 	
 	return "ok"
+
+
+
+
+PLOTLY_LOGO = "https://www.portaldosjornalistas.com.br/wp-content/uploads/2019/09/Abraji.png"
+
+
+navbar = dbc.Navbar(
+        [
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src = PLOTLY_LOGO, height = "70px"), ),
+                        
+                        dbc.Col(
+             dbc.NavbarBrand("App Title", style = {'color':'black', 'fontSize':'25px','fontFamily':'Times New Roman'}
+                            ),
+             ),
+
+                    ],
+                    align="center",
+                    className="g-10",
+                ),
+            
+            dbc.Row(
+            [
+        dbc.Col(
+        dbc.Button(id = 'button', children = "Click Me!", color = "primary"), 
+            )        
+    ],
+            # add a top margin to make things look nice when the navbar
+            # isn't expanded (mt-3) remove the margin on medium or
+            # larger screens (mt-md-0) when the navbar is expanded.
+            # keep button and search box on same row (flex-nowrap).
+            # align everything on the right with left margin (ms-auto).
+     className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
+)
+            
+         ],
+)
+
+
+app.layout = html.Div(id = 'parent', children = [navbar])
+
+
+if __name__ == "__main__":
+    app.run_server()
+
+
