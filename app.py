eb37237aa6c082a6f8e66f65fd5508d0611808d5
Reinaldo Chaves <reichaves@gmail.com>
2022-01-11 16:45:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,36 +15,6 @@ import datetime
 app = dash.Dash(external_stylesheets = [ dbc.themes.FLATLY],)
 
 
-@app.route("/telegram", methods=["POST"])
-
-def telegram():
-	# Processa mensagem
-	update = request.json
-	chat_id = update["message"]["chat"]["id"]
-	text = update["message"]["text"].lower()
-	
-	if text in ['oi', 'olá', 'ola']:
-		answer = "Oi, como vai?!"
-	elif text in ['bom dia', 'boa tarde', 'boa noite']:
-		answer = text
-	elif "jornalismo" in text:
-		answer = "Em breve..."
-	else:
-		answer = "Não entendi!!!"
-	
-  	# Responde mensagem
-	token = os.environ["TELEGRAM_TOKEN"]
-	message = {"chat_id": chat_id, "text": answer}
-	url = f"https://api.telegram.org/bot{token}/sendMessage"
-	response = requests.post(url, data=message)
-	
-	if response.json()["ok"] == False:
-		raise RuntimeError("Erro ao responder API do Telegram")
-	
-	return "ok"
-
-
-
 
 PLOTLY_LOGO = "https://www.portaldosjornalistas.com.br/wp-content/uploads/2019/09/Abraji.png"
 
