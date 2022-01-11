a1efa91b66fe0760b153469f7ed674af9ff2d04a
Reinaldo Chaves <reichaves@gmail.com>
2022-01-11 17:00:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,35 @@ navbar = dbc.Navbar(
 
 app.layout = html.Div(id = 'parent', children = [navbar])
 
+@app.route("/telegram", methods=["POST"])
+
+def telegram():
+        # Processa mensagem
+	update = request.json
+	chat_id = update["message"]["chat"]["id"]
+	text = update["message"]["text"].lower()
+	
+	if text in ['oi', 'olá', 'ola']:
+		answer = "Oi, como vai?!"
+	elif text in ['bom dia', 'boa tarde', 'boa noite']:
+		answer = text
+	elif "jornalismo" in text:
+		answer = "Em breve..."
+	else:
+		answer = "Não entendi!!!"
+	
+  	# Responde mensagem
+	token = os.environ["TELEGRAM_TOKEN"]
+	message = {"chat_id": chat_id, "text": answer}
+	url = f"https://api.telegram.org/bot{token}/sendMessage"
+	response = requests.post(url, data=message)
+	
+	if response.json()["ok"] == False:
+		raise RuntimeError("Erro ao responder API do Telegram")
+	
+	return "ok"
+
+
 
 if __name__ == "__main__":
     app.run_server()
